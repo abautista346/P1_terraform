12dca4d189223d8d1cc45904c09ea54c0dcdb366
abautista346 <abautistausac@gmail.com>
2023-05-24 15:16:52 -0600

    arreglo

--- provider.tf
+++ provider.tf
@@ -18,16 +18,6 @@ terraform {
 
 //configuration my AWS provider
 provider "aws" {
-<<<<<<< HEAD
-
     //region  =   "us-east-1"
-
-=======
-<<<<<<< HEAD
-    ///region  =   "us-east-1" //old region changed  because here exist a problem with the number of resources
-=======
-    //region  =   "us-east-1"
->>>>>>> 4f841aa5040f5bcc5ec1e1a184f56bfa1f88143b
->>>>>>> 7d959339ed48bd879bbe41368bad0408c93a1b59
     region  = "us-west-2"
 }
